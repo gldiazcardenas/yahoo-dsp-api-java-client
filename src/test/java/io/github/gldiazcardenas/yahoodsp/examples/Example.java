7dae31e2415a6f21a6c383affe7224f47bf713cc
Gabriel Diaz <gabriel.leonardo.diaz@gmail.com>
2022-09-17 06:03:46 -0500

    Added some more constants for testing

--- src/test/java/io/github/gldiazcardenas/yahoodsp/examples/Example.java
+++ src/test/java/io/github/gldiazcardenas/yahoodsp/examples/Example.java
@@ -1,15 +1,21 @@
 package io.github.gldiazcardenas.yahoodsp.examples;
 
+import io.github.gldiazcardenas.yahoodsp.Payloads;
 import io.github.gldiazcardenas.yahoodsp.client.DspApi;
+import io.github.gldiazcardenas.yahoodsp.client.model.Authentication;
 
 /**
  * @author Gabriel Diaz, Aug 25th 2022.
  */
 public interface Example {
 
-    String CLIENT_ID = "<<INSERT>>";
-    String CLIENT_SECRET = "<<INSERT>>";
-    String ACCESS_TOKEN = "<<INSERT>>";
+    Long ADVERTISER_ID = 0L;
+    Long SEAT_ID = 0L;
+    Long CATEGORY_ID = 0L;
+    String CLIENT_ID = "<<CLIENT_ID>>";
+    String CLIENT_SECRET = "<<CLIENT_SECRET>>";
+    String ACCESS_TOKEN = "<<ACCESS_TOKEN>>";
+    Authentication AUTH = Payloads.authentication().setAccessToken(ACCESS_TOKEN);
     DspApi API = DspApi.builder().build();
 
 }
