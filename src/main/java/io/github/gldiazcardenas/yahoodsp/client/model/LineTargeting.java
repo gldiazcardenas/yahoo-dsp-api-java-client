06bfa6bc2ad0fd2d69f44d8327792887848f2589
Gabriel Diaz <gabriel.leonardo.diaz@gmail.com>
2022-10-03 10:49:30 -0500

    Added more targeting settings

--- src/main/java/io/github/gldiazcardenas/yahoodsp/client/model/LineTargeting.java
+++ src/main/java/io/github/gldiazcardenas/yahoodsp/client/model/LineTargeting.java
@@ -39,4 +39,211 @@ public class LineTargeting {
     private Boolean enableViewabilityFreqCap;
     private List<LineTargetingType> types;
 
+    public List<Gender> getGenders() {
+        return genders;
+    }
+
+    public void setGenders(List<Gender> genders) {
+        this.genders = genders;
+    }
+
+    public List<AgeRange> getAges() {
+        return ages;
+    }
+
+    public void setAges(List<AgeRange> ages) {
+        this.ages = ages;
+    }
+
+    public Integer getFrequencyCapValue() {
+        return frequencyCapValue;
+    }
+
+    public void setFrequencyCapValue(Integer frequencyCapValue) {
+        this.frequencyCapValue = frequencyCapValue;
+    }
+
+    public FrequencyCapPeriodType getFrequencyCapPeriod() {
+        return frequencyCapPeriod;
+    }
+
+    public void setFrequencyCapPeriod(FrequencyCapPeriodType frequencyCapPeriod) {
+        this.frequencyCapPeriod = frequencyCapPeriod;
+    }
+
+    public Boolean getServeUnknownUser() {
+        return serveUnknownUser;
+    }
+
+    public void setServeUnknownUser(Boolean serveUnknownUser) {
+        this.serveUnknownUser = serveUnknownUser;
+    }
+
+    public List<DeviceType> getDevices() {
+        return devices;
+    }
+
+    public void setDevices(List<DeviceType> devices) {
+        this.devices = devices;
+    }
+
+    public List<LanguageType> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(List<LanguageType> languages) {
+        this.languages = languages;
+    }
+
+    public List<InventoryType> getInventoryTypes() {
+        return inventoryTypes;
+    }
+
+    public void setInventoryTypes(List<InventoryType> inventoryTypes) {
+        this.inventoryTypes = inventoryTypes;
+    }
+
+    public List<OsType> getOs() {
+        return os;
+    }
+
+    public void setOs(List<OsType> os) {
+        this.os = os;
+    }
+
+    public List<BrowserType> getBrowsers() {
+        return browsers;
+    }
+
+    public void setBrowsers(List<BrowserType> browsers) {
+        this.browsers = browsers;
+    }
+
+    public List<OsType> getMobileOs() {
+        return mobileOs;
+    }
+
+    public void setMobileOs(List<OsType> mobileOs) {
+        this.mobileOs = mobileOs;
+    }
+
+    public List<BrowserType> getMobileBrowsers() {
+        return mobileBrowsers;
+    }
+
+    public void setMobileBrowsers(List<BrowserType> mobileBrowsers) {
+        this.mobileBrowsers = mobileBrowsers;
+    }
+
+    public List<AdInitiationType> getAdInitiations() {
+        return adInitiations;
+    }
+
+    public void setAdInitiations(List<AdInitiationType> adInitiations) {
+        this.adInitiations = adInitiations;
+    }
+
+    public List<PositionType> getPositions() {
+        return positions;
+    }
+
+    public void setPositions(List<PositionType> positions) {
+        this.positions = positions;
+    }
+
+    public List<PlayerSizeType> getPlayerSizes() {
+        return playerSizes;
+    }
+
+    public void setPlayerSizes(List<PlayerSizeType> playerSizes) {
+        this.playerSizes = playerSizes;
+    }
+
+    public List<Deal> getDealsIncluded() {
+        return dealsIncluded;
+    }
+
+    public void setDealsIncluded(List<Deal> dealsIncluded) {
+        this.dealsIncluded = dealsIncluded;
+    }
+
+    public List<Publisher> getPublishersIncluded() {
+        return publishersIncluded;
+    }
+
+    public void setPublishersIncluded(List<Publisher> publishersIncluded) {
+        this.publishersIncluded = publishersIncluded;
+    }
+
+    public List<Publisher> getPublishersExcluded() {
+        return publishersExcluded;
+    }
+
+    public void setPublishersExcluded(List<Publisher> publishersExcluded) {
+        this.publishersExcluded = publishersExcluded;
+    }
+
+    public List<Geo> getGeosIncluded() {
+        return geosIncluded;
+    }
+
+    public void setGeosIncluded(List<Geo> geosIncluded) {
+        this.geosIncluded = geosIncluded;
+    }
+
+    public List<Geo> getGeosExcluded() {
+        return geosExcluded;
+    }
+
+    public void setGeosExcluded(List<Geo> geosExcluded) {
+        this.geosExcluded = geosExcluded;
+    }
+
+    public List<Segment> getSegmentsIncluded() {
+        return segmentsIncluded;
+    }
+
+    public void setSegmentsIncluded(List<Segment> segmentsIncluded) {
+        this.segmentsIncluded = segmentsIncluded;
+    }
+
+    public List<Segment> getSegmentsExcluded() {
+        return segmentsExcluded;
+    }
+
+    public void setSegmentsExcluded(List<Segment> segmentsExcluded) {
+        this.segmentsExcluded = segmentsExcluded;
+    }
+
+    public Boolean getEnableSegmentExpansion() {
+        return enableSegmentExpansion;
+    }
+
+    public void setEnableSegmentExpansion(Boolean enableSegmentExpansion) {
+        this.enableSegmentExpansion = enableSegmentExpansion;
+    }
+
+    public Boolean getAllCountriesTargeted() {
+        return isAllCountriesTargeted;
+    }
+
+    public void setAllCountriesTargeted(Boolean allCountriesTargeted) {
+        isAllCountriesTargeted = allCountriesTargeted;
+    }
+
+    public Boolean getEnableViewabilityFreqCap() {
+        return enableViewabilityFreqCap;
+    }
+
+    public void setEnableViewabilityFreqCap(Boolean enableViewabilityFreqCap) {
+        this.enableViewabilityFreqCap = enableViewabilityFreqCap;
+    }
+
+    public List<LineTargetingType> getTypes() {
+        return types;
+    }
+
+    public void setTypes(List<LineTargetingType> types) {
+        this.types = types;
+    }
 }
--- src/main/java/io/github/gldiazcardenas/yahoodsp/client/model/LineTargetingRequest.java
+++ src/main/java/io/github/gldiazcardenas/yahoodsp/client/model/LineTargetingRequest.java
@@ -38,4 +38,203 @@ public class LineTargetingRequest {
     private SegmentTargeting segments;
     private List<LineTargetingType> types;
 
+    public List<Gender> getGenders() {
+        return genders;
+    }
+
+    public void setGenders(List<Gender> genders) {
+        this.genders = genders;
+    }
+
+    public List<AgeRange> getAges() {
+        return ages;
+    }
+
+    public void setAges(List<AgeRange> ages) {
+        this.ages = ages;
+    }
+
+    public Integer getThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(Integer threshold) {
+        this.threshold = threshold;
+    }
+
+    public FrequencyCapPeriodType getFrequencyCapPeriod() {
+        return frequencyCapPeriod;
+    }
+
+    public void setFrequencyCapPeriod(FrequencyCapPeriodType frequencyCapPeriod) {
+        this.frequencyCapPeriod = frequencyCapPeriod;
+    }
+
+    public FrequencyCapPeriodType getFrequencyCapValue() {
+        return frequencyCapValue;
+    }
+
+    public void setFrequencyCapValue(FrequencyCapPeriodType frequencyCapValue) {
+        this.frequencyCapValue = frequencyCapValue;
+    }
+
+    public Boolean getServeUnknownUser() {
+        return serveUnknownUser;
+    }
+
+    public void setServeUnknownUser(Boolean serveUnknownUser) {
+        this.serveUnknownUser = serveUnknownUser;
+    }
+
+    public DayPartingTargeting getDayParting() {
+        return dayParting;
+    }
+
+    public void setDayParting(DayPartingTargeting dayParting) {
+        this.dayParting = dayParting;
+    }
+
+    public List<String> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(List<String> languages) {
+        this.languages = languages;
+    }
+
+    public List<InventoryType> getInventoryTypes() {
+        return inventoryTypes;
+    }
+
+    public void setInventoryTypes(List<InventoryType> inventoryTypes) {
+        this.inventoryTypes = inventoryTypes;
+    }
+
+    public List<BotType> getBots() {
+        return bots;
+    }
+
+    public void setBots(List<BotType> bots) {
+        this.bots = bots;
+    }
+
+    public List<DeviceType> getDeviceTypes() {
+        return deviceTypes;
+    }
+
+    public void setDeviceTypes(List<DeviceType> deviceTypes) {
+        this.deviceTypes = deviceTypes;
+    }
+
+    public List<OsType> getOs() {
+        return os;
+    }
+
+    public void setOs(List<OsType> os) {
+        this.os = os;
+    }
+
+    public List<BrowserType> getBrowsers() {
+        return browsers;
+    }
+
+    public void setBrowsers(List<BrowserType> browsers) {
+        this.browsers = browsers;
+    }
+
+    public List<OsType> getMobileOs() {
+        return mobileOs;
+    }
+
+    public void setMobileOs(List<OsType> mobileOs) {
+        this.mobileOs = mobileOs;
+    }
+
+    public List<BrowserType> getMobileBrowsers() {
+        return mobileBrowsers;
+    }
+
+    public void setMobileBrowsers(List<BrowserType> mobileBrowsers) {
+        this.mobileBrowsers = mobileBrowsers;
+    }
+
+    public List<AdInitiationType> getAdInitiations() {
+        return adInitiations;
+    }
+
+    public void setAdInitiations(List<AdInitiationType> adInitiations) {
+        this.adInitiations = adInitiations;
+    }
+
+    public List<PositionType> getPositions() {
+        return positions;
+    }
+
+    public void setPositions(List<PositionType> positions) {
+        this.positions = positions;
+    }
+
+    public List<PlayerSizeType> getPlayerSizes() {
+        return playerSizes;
+    }
+
+    public void setPlayerSizes(List<PlayerSizeType> playerSizes) {
+        this.playerSizes = playerSizes;
+    }
+
+    public List<VideoContentLengthType> getVideoContentLength() {
+        return videoContentLength;
+    }
+
+    public void setVideoContentLength(List<VideoContentLengthType> videoContentLength) {
+        this.videoContentLength = videoContentLength;
+    }
+
+    public DealTargeting getDeals() {
+        return deals;
+    }
+
+    public void setDeals(DealTargeting deals) {
+        this.deals = deals;
+    }
+
+    public List<Long> getPublishers() {
+        return publishers;
+    }
+
+    public void setPublishers(List<Long> publishers) {
+        this.publishers = publishers;
+    }
+
+    public Boolean getIncludePublishers() {
+        return includePublishers;
+    }
+
+    public void setIncludePublishers(Boolean includePublishers) {
+        this.includePublishers = includePublishers;
+    }
+
+    public GeoTargeting getGeos() {
+        return geos;
+    }
+
+    public void setGeos(GeoTargeting geos) {
+        this.geos = geos;
+    }
+
+    public SegmentTargeting getSegments() {
+        return segments;
+    }
+
+    public void setSegments(SegmentTargeting segments) {
+        this.segments = segments;
+    }
+
+    public List<LineTargetingType> getTypes() {
+        return types;
+    }
+
+    public void setTypes(List<LineTargetingType> types) {
+        this.types = types;
+    }
 }
