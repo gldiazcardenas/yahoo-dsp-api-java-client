ff715c076ad2adcdb60d0d00bb1d2d032d0ff33d
Gabriel Diaz <gabriel.leonardo.diaz@gmail.com>
2022-10-05 11:31:27 -0500

    Fixed media type

--- src/main/java/io/github/gldiazcardenas/yahoodsp/client/model/MediaType.java
+++ src/main/java/io/github/gldiazcardenas/yahoodsp/client/model/MediaType.java
@@ -1,10 +1,24 @@
 package io.github.gldiazcardenas.yahoodsp.client.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 /**
  * @author Gabriel Diaz, Sep 21th 2022.
  */
 public enum MediaType {
     DISPLAY,
     VIDEO,
-    AUDIO
+    AUDIO,
+    NATIVE,
+    UNKNOWN;
+
+    @JsonCreator
+    public static MediaType fromName(String name) {
+        for (MediaType type : values()) {
+            if (type.name().equals(name)) {
+                return type;
+            }
+        }
+        return UNKNOWN;
+    }
 }
