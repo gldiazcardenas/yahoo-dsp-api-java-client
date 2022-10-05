8c8ffc454d73e053e0212f5aa5a724ae89b25b87
Gabriel Diaz <gabriel.leonardo.diaz@gmail.com>
2022-10-05 11:21:10 -0500

    Fixed creative details

--- src/main/java/io/github/gldiazcardenas/yahoodsp/client/model/CreativeDetails.java
+++ src/main/java/io/github/gldiazcardenas/yahoodsp/client/model/CreativeDetails.java
@@ -15,7 +15,77 @@ public class CreativeDetails {
     private Integer duration;
     private CreativeImages images;
     private List<CreativeMedia> creativeMedia;
+    private String title;
+    private String companyName;
+    private String description;
+    private String callToAction;
 
+    public Integer getDuration() {
+        return duration;
+    }
 
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
 
+    public CreativeImages getImages() {
+        return images;
+    }
+
+    public void setImages(CreativeImages images) {
+        this.images = images;
+    }
+
+    public List<CreativeMedia> getCreativeMedia() {
+        return creativeMedia;
+    }
+
+    public void setCreativeMedia(List<CreativeMedia> creativeMedia) {
+        this.creativeMedia = creativeMedia;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCallToAction() {
+        return callToAction;
+    }
+
+    public void setCallToAction(String callToAction) {
+        this.callToAction = callToAction;
+    }
+
+    @Override
+    public String toString() {
+        return "CreativeDetails{" +
+                "duration=" + duration +
+                ", images=" + images +
+                ", creativeMedia=" + creativeMedia +
+                ", title='" + title + '\'' +
+                ", companyName='" + companyName + '\'' +
+                ", description='" + description + '\'' +
+                ", callToAction='" + callToAction + '\'' +
+                '}';
+    }
 }
