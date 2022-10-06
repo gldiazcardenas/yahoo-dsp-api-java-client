e9cb32d060845f6b9bae4869632ad0688bb1c470
Gabriel Diaz <gabriel.leonardo.diaz@gmail.com>
2022-10-06 18:02:26 -0500

    Added some report examples

---
+++ src/test/java/io/github/gldiazcardenas/yahoodsp/examples/reporting/CreateReportBrokenDownByPublisherWithClicksSpentImpressions.java
@@ -0,0 +1,59 @@
+package io.github.gldiazcardenas.yahoodsp.examples.reporting;
+
+import io.github.gldiazcardenas.yahoodsp.client.model.ReportDateType;
+import io.github.gldiazcardenas.yahoodsp.client.model.ReportDimensionType;
+import io.github.gldiazcardenas.yahoodsp.client.model.ReportFilterOption;
+import io.github.gldiazcardenas.yahoodsp.client.model.ReportFilterOptionValue;
+import io.github.gldiazcardenas.yahoodsp.client.model.ReportIntervalType;
+import io.github.gldiazcardenas.yahoodsp.client.model.ReportMetricType;
+import io.github.gldiazcardenas.yahoodsp.client.model.ReportOption;
+import io.github.gldiazcardenas.yahoodsp.client.model.ReportRequest;
+import io.github.gldiazcardenas.yahoodsp.examples.Example;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+/**
+ * @author Gabriel Diaz, Oct 06th 2022.
+ */
+public class CreateReportBrokenDownByPublisherWithClicksSpentImpressions implements Example {
+
+    public static void main(String[] arg) {
+        ReportFilterOptionValue filterValue = new ReportFilterOptionValue();
+        filterValue.setId(String.valueOf(CAMPAIGN_ID));
+
+        ReportFilterOption filterOption = new ReportFilterOption();
+        filterOption.setDimensionTypeId(ReportDimensionType.CAMPAIGN);
+        filterOption.setExcluded(false);
+        filterOption.setIncludeValues(new ArrayList<>());
+        filterOption.getIncludeValues().add(filterValue);
+
+        ReportOption options = new ReportOption();
+        options.setAccountIds(new ArrayList<>());
+        options.getAccountIds().add(ADVERTISER_ID);
+        options.setDimensionTypeIds(new ArrayList<>());
+        options.getDimensionTypeIds().add(ReportDimensionType.ADVERTISER);
+        options.getDimensionTypeIds().add(ReportDimensionType.CAMPAIGN);
+        options.getDimensionTypeIds().add(ReportDimensionType.LINE);
+        options.getDimensionTypeIds().add(ReportDimensionType.PUBLISHER);
+        options.getDimensionTypeIds().add(ReportDimensionType.DOMAIN);
+        options.getDimensionTypeIds().add(ReportDimensionType.SUBDOMAIN);
+        options.setMetricTypeIds(new ArrayList<>());
+        options.getMetricTypeIds().add(ReportMetricType.CLICKS);
+        options.getMetricTypeIds().add(ReportMetricType.IMPRESSIONS);
+        options.getMetricTypeIds().add(ReportMetricType.ADVERTISER_SPENDING);
+        options.setFilterOptions(new ArrayList<>());
+        options.getFilterOptions().add(filterOption);
+
+        ReportRequest request = new ReportRequest();
+        request.setStartDate(LocalDate.now().atStartOfDay());
+        request.setEndDate(LocalDateTime.now());
+        request.setDateTypeId(ReportDateType.CUSTOM);
+        request.setIntervalTypeId(ReportIntervalType.DAY);
+        request.setReportOption(options);
+
+        API.getReportService().create(AUTH, request);
+    }
+
+}
